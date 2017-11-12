5d16a1f34d6ac10d7d1501fe9bb09653bf6b78a4
jekyllx <anand.pal2050@gmail.com>
2017-11-12 23:32:03 +0530

    initial commit

--- inc/signup.inc.php
+++ inc/signup.inc.php
@@ -11,7 +11,56 @@ if (isset($_POST['submit'])) {
 	$email = mysql_real_escape_string($conn, $_POST['user_email']);
 	$pwd = mysql_real_escape_string($conn, $_POST['pwd']);
 
+	// Error Handlers
+
+	// checkfor empty filds
+
+	if (empty($uid) || empty($first) || empty($last) || empty($email) || empty($pwd)) {
+				header("location: ../signup.php?signup=empty");
+				exit();	
+	}
+	else{
+
+
+		// check input characters are valid.
+		if (!preg_match("/^[a-zA-Z]*$/", $first) || !preg_match("/^[a-zA-Z]*$/", $last)) {
+			    header("location: ../signup.php?signup=invalid");
+				exit();
+		}
+		esle{
+
+			// check if email is valid
+			if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+				header("location: ../signup.php?signup=email");
+				exit();
+			}
+			else{
+				$sql = "SELECT * FROM  register_user WHERE user_uid = '$uid'";
+				$result = mysql_query($conn, $sql);
+				$resultCheck = mysqli_num_rows($result);
+
+				if ($resultCheck > 0) {
+					header("location: ../signup.php?signup=userExit");
+					exit();
+				}
+				else{
+					// hashing a password
+					$hashedPwd = password_hash($pwd, PASSWORD_DEFAULT);
+
+					// insert the user into the database
+					$sql ="INSERT INTO register_user (user_first_name, user_last_name, user_email_id, user_uid, user_password) VALUES ('$first, $last, $email, $email, $hashedPwd');";
+
+					$result = mysql_query($conn, $sql);
+					header("location: ../signup.php?signup=success");
+				    exit();
+				}
+			}
+		}
+	}
+
 }
+
+
 else{
 	header("location: ../signup.php");
 	exit();
